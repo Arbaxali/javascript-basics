049e4a2f30e05e51f53f6144c75c887ab606bb1b
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2021-07-19 22:33:49 +0530

    VER-3

---
+++ arrays.js
@@ -0,0 +1,11 @@
+let arr=[];
+let arr1 =[];
+let st = "hello"
+st1= st.split('')
+arr.push(st1)
+//console.log(arr);
+
+for(let i=arr.lenght -1; i>=0; i--){
+    arr1.push(arr[i]);
+}
+console.log(arr1);
