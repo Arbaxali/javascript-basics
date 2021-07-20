be3b116359a69153206b29db4f2ceac246ab250b
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2021-07-20 21:45:24 +0530

    4

---
+++ trello.js
@@ -0,0 +1,9 @@
+todo =[]
+
+todo.push("im arbazali")
+todo.push('i gotta go')
+todo.push('hello')
+
+for( let index= todo.length-1; index>= 0; index--){
+    console.log(`my todo list : ${todo[index]}`)
+}
---
+++ variable.js
@@ -0,0 +1,6 @@
+let a="100"
+let b="200"
+let name1 ="arbax"
+
+
+console.log( name1 + ' ' +a +' '+ b)
